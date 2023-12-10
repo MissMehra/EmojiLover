8013efd830bd82294e17d130999817cbd8669836
MissMehra <mehrapriyanka255@gmail.com>
2023-12-10 07:56:51 +0530

    Initial Commit

---
+++ EmojiLover/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  EmojiLover
+//
+//  Created by Priyanka Mehra on 30/11/23.
+//
+
+import SwiftUI
+enum Emoji: String , CaseIterable {
+    case 😀,👻,🥰,📩,👹
+    
+}
+
+struct ContentView: View {
+    @State var selection: Emoji = .😀
+
+    var body: some View {
+        NavigationView{VStack {
+            Text(selection.rawValue)
+                .font(.system(size: 200))
+            
+            Picker("Select Emoji", selection: $selection) {
+                ForEach(Emoji.allCases, id: \.self) { Emoji in
+                    Text(Emoji.rawValue)
+                }
+            }
+            .pickerStyle(.segmented)
+        }
+        .navigationTitle("Emoji Lovers!")
+        }.padding()
+    }
+}
+
+struct ContentView_previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Priyanka Mehra on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
